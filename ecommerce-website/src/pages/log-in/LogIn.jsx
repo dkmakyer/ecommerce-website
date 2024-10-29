6cc63cb95144320697b1ea27424703585c8ede6d
David Akyer <kwasiakyer2105@gmail.com>
2024-10-29 10:49:04 +0000

    updated SignUp.jsx
    created a state for the signup and login pages

--- ecommerce-website/src/pages/log-in/LogIn.jsx
+++ ecommerce-website/src/pages/log-in/LogIn.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
 import "./LogIn.css"
 import images from "../../images";
 
 const LogIn = () => {
   const { miniMart} = images;
+  const [logInData, setLogInData] = useState({email:"", password:""});
+  
+  function fillLogIn(event){
+    const {name, value} = event.target;
+    setLogInData(prev => ({...prev, [name]: value}));
+  }
+
+  function submitLogIn(event){
+    event.preventDefault();
+    alert(`log-in-form Submitted: ${logInData.email}, ${logInData.password}`);
+    //send form to backend database
+  }
+  
   return (
     <>
       <div className="log-in-container">
@@ -13,12 +26,12 @@ const LogIn = () => {
         <div className="log-in-form">
           <h2>Log in to Agbogbloshie</h2>
           <h4>Enter your details below</h4>
-          <form action="">
-            <input type="text" placeholder='Email or Phone Number'/>
-            <input type="password" placeholder='Password'/>
+          <form onSubmit={submitLogIn}>
+            <input type="text" placeholder='Email or Phone Number' name="email" value={logInData.email} onChange={fillLogIn}/>
+            <input type="password" placeholder='Password' name="password" value={logInData.password} onChange={fillLogIn}/>
             <div className="log-in-buttons">
-              <button className='log-in-button'>Log-In</button>
-              <button className='forget-password-button'>forget-password?</button>
+              <button type='submit' className='log-in-button'>Log-In</button>
+              <button type='submit' className='forget-password-button'>forget-password?</button>
             </div>
           </form>
         </div>
--- ecommerce-website/src/pages/sign-up/Sign-up.jsx
+++ ecommerce-website/src/pages/sign-up/Sign-up.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import {useState} from 'react'
 import "./Sign-up.css"
 import images from "../../images";
 import { NavLink } from 'react-router-dom';
 
 const SignUp = () => {
-  const { shopping, miniMart, google } = images;
+  const { shopping, google } = images;
+
+  const [signUpData, setSignUPData] = useState({name:"", email:"", password:""});
+  
+  function fillSignUp(event){
+    const {name, value} = event.target;
+    setSignUPData(prevFormData => ({...prevFormData, [name]: value}));
+  }
+  function submitSignUp(event){
+    event.preventDefault();
+    alert(`form submitted: ${signUpData.name}, ${signUpData.email}, ${signUpData.password}`);
+    //send form to backend database
+  }
+
   return (
     <>
       <div className="sign-up-container">
@@ -14,13 +27,13 @@ const SignUp = () => {
         <div className="form">
           <h2>Create an Account</h2>
           <h4>Enter your details below</h4>
-          <form action="">
-            <input type="text" placeholder='Name'/>
-            <input type="text" placeholder='Email or Phone Number'/>
-            <input type="password" placeholder='Password'/>
+          <form action="" onSubmit={submitSignUp}>
+            <input type="text" placeholder='Name' name="name" value={signUpData.name} onChange={fillSignUp}/>
+            <input type="text" placeholder='Email or Phone Number' name="email" value={signUpData.email} onChange={fillSignUp}/>
+            <input type="password" placeholder='Password' name="password" value={signUpData.password} onChange={fillSignUp}/>
             <div className="buttons">
-              <button className='create-account-button'>Create Account</button>
-              <button className='google-sign-in-button'><img src={google} alt="google image" /><span>Sign in with Google</span></button>
+              <button type='submit' className='create-account-button'>Create Account</button>
+              <button type='submit' className='google-sign-in-button'><img src={google} alt="google image" /><span>Sign in with Google</span></button>
             </div>
             <div className="log-in">
               <p>Already have an account?</p>
