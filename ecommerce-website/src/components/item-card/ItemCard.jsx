cb884f0596ccef0d69e78dbf644bb3a018156a6e
David Akyer <kwasiakyer2105@gmail.com>
2024-11-06 11:17:45 +0000

    created ItemCard.jsx
    Created a component for the individual items to improve code readability

---
+++ ecommerce-website/src/components/item-card/ItemCard.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./ItemCard.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight, faEye, faHeart, faMapPin, faStar } from "@fortawesome/free-solid-svg-icons";
+
+const ItemCard = () => {
+  return (
+    <>
+        <div className="item">
+          <div className="discount-area">
+            <p>-40%</p>
+            <div className="seen">
+              <button className="favorite">
+                <FontAwesomeIcon className="favorite" icon={faHeart} />
+              </button>
+              <button className="viewed">
+                <FontAwesomeIcon className="viewed" icon={faEye} />
+              </button>
+            </div>
+            <button className="add-item">Add to Cart</button>
+          </div>
+          <div className="item-info">
+            <h4>game pad</h4>
+            <p style={{ color: "red" }}>
+              $300{" "}
+              <span
+                style={{
+                  color: "grey",
+                  textDecoration: "line-through",
+                  marginLeft: "10px",
+                }}
+              >
+                $350
+              </span>
+            </p>
+            <div className="stars">
+              <FontAwesomeIcon className="star" icon={faStar} />
+              <FontAwesomeIcon className="star" icon={faStar} />
+              <FontAwesomeIcon className="star" icon={faStar} />
+              <FontAwesomeIcon className="star" icon={faStar} />
+              <FontAwesomeIcon className="star" icon={faStar} />
+              <p style={{ color: "grey" }}>(90)</p>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default ItemCard;
--- ecommerce-website/src/pages/home/Home.jsx
+++ ecommerce-website/src/pages/home/Home.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./Home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft, faArrowRight, faCaretRight, faEye, faHeart, faMapPin, faStar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faArrowRight,
+  faCaretRight,
+  faEye,
+  faHeart,
+  faMapPin,
+  faStar,
+} from "@fortawesome/free-solid-svg-icons";
 import images from "../../images";
+import ItemCard from "../../components/item-card/ItemCard";
 
 const Home = () => {
   const { pepsi } = images;
@@ -12,11 +21,19 @@ const Home = () => {
         <div className="ads">
           <div className="ads-links">
             <ul>
-              <li >
-                Women's Fashion <FontAwesomeIcon className="caret-link-one" icon={faCaretRight} />
+              <li>
+                Women's Fashion{" "}
+                <FontAwesomeIcon
+                  className="caret-link-one"
+                  icon={faCaretRight}
+                />
               </li>
-              <li >
-                Men's Fashion <FontAwesomeIcon className="caret-link-two" icon={faCaretRight} />
+              <li>
+                Men's Fashion{" "}
+                <FontAwesomeIcon
+                  className="caret-link-two"
+                  icon={faCaretRight}
+                />
               </li>
               <li>Electronics</li>
               <li>Home & Lifestyle</li>
@@ -39,45 +56,55 @@ const Home = () => {
         </div>
         <div className="flash-sales-container">
           <div className="today">
-            <FontAwesomeIcon style={{fontSize: "30px", marginRight: "10px"}}icon={faMapPin}/><p>Today's</p>
+            <FontAwesomeIcon
+              style={{ fontSize: "30px", marginRight: "10px" }}
+              icon={faMapPin}
+            />
+            <p>Today's</p>
           </div>
           <div className="timer">
             <h1>Flash Sales</h1>
             <div className="countdown">
-              <p><span className="reduce-time-size">Days</span> <span>03</span> <span className="colon">:</span></p>
-              <p><span className="reduce-time-size">Hours</span> <span>23</span> <span className="colon">:</span></p>
-              <p><span className="reduce-time-size">Minutes</span> <span>19</span> <span className="colon">:</span></p>
-              <p><span className="reduce-time-size">Seconds</span> <span>56</span></p>
+              <p>
+                <span className="reduce-time-size">Days</span> <span>03</span>{" "}
+                <span className="colon">:</span>
+              </p>
+              <p>
+                <span className="reduce-time-size">Hours</span> <span>23</span>{" "}
+                <span className="colon">:</span>
+              </p>
+              <p>
+                <span className="reduce-time-size">Minutes</span>{" "}
+                <span>19</span> <span className="colon">:</span>
+              </p>
+              <p>
+                <span className="reduce-time-size">Seconds</span>{" "}
+                <span>56</span>
+              </p>
             </div>
             <div className="scroll">
-              <div className="left-arrow"><FontAwesomeIcon  icon={faArrowLeft}/></div>
-              <div className="right-arrow"><FontAwesomeIcon icon={faArrowRight}/></div>
+              <div className="left-arrow">
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </div>
+              <div className="right-arrow">
+                <FontAwesomeIcon icon={faArrowRight} />
+              </div>
             </div>
           </div>
+
           <div className="items-container">
-            <div className="item">
-              <div className="discount-area">
-                <p>-40%</p>
-                <div className="seen">
-                  <button  className="favorite"><FontAwesomeIcon className="favorite" icon={faHeart}/></button>
-                  <button  className="viewed"><FontAwesomeIcon className="viewed" icon={faEye}/></button>
-                </div>
-                <button className="add-item">Add to Cart</button>
-              </div>
-              <div className="item-info">
-                <h4>game pad</h4>
-                <p>$300 <span>$350</span></p>
-                <div className="stars">
-                  <FontAwesomeIcon className="star" icon={faStar}/>
-                  <FontAwesomeIcon className="star" icon={faStar}/>
-                  <FontAwesomeIcon className="star" icon={faStar}/>
-                  <FontAwesomeIcon className="star" icon={faStar}/>
-                  <FontAwesomeIcon className="star" icon={faStar}/>
-                  <p>(90)</p>
-                </div>
-              </div>
-            </div>
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
+            <ItemCard />
           </div>
+          <button className="view-all-products-button">
+            View All Products
+          </button>
         </div>
       </div>
     </>
