a5cb2f28ea41e44928a7b846dec01dda2b22ab12
dkmakyer <kwasiakyer2105@gmail.com>
2024-12-11 10:41:49 +0000

    updated About.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./Contact.css"
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
+    <>
       
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
